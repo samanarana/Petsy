a5c35348709bbb82cf7ae1fb0f03c1a3eedf201c
Chris <andre89gomez@gmail.com>
2023-10-11 17:46:25 -0700

    updated tables and added Favorites

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,5 @@
 from .db import db, environment, SCHEMA
 from .user import User
 from .review import Review
-from .cartitems import cartitems
-from .products import Products
+from .cartItem import cartItem
+from .product import Product
