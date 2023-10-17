2282a2cc73c4c7fe6c6d346319f741a2278c1680
Chris <andre89gomez@gmail.com>
2023-10-17 12:14:51 -0700

    Updated some bugs related to displaying favorites, still needs work

--- react-app/src/components/FavoritesPage/index.js
+++ react-app/src/components/FavoritesPage/index.js
@@ -3,21 +3,31 @@ import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import FavoriteTile from './FavoriteTile';
 import { fetchAllFavoritesThunk } from '../../store/favorite';
+import { fetchAllProductsThunk } from '../../store/product';
 
 const FavoriteTileList = () => {
     const dispatch = useDispatch();
     const { userId } = useParams()
 
     useEffect(() => {
+        dispatch(fetchAllProductsThunk())
         dispatch(fetchAllFavoritesThunk(userId))
     }, [dispatch, userId]);
 
     // Get favorite data from redux state
     const favorites = useSelector(state => state.favorite.favorites) || [];
+    // console.log('----------------------------------------------', favorites)
+    const products = useSelector(state => state.product.products) || [];
+    const favoriteProductIds = favorites.map(favorite => favorite.productId);
+
+    const favoriteProducts = products.filter(product => favoriteProductIds.includes(product.id));
+
+
+
 
     return (
-        <div className='favorite-tile-list'>
-            {favorites.map(favorite => (
+        <div className='product-tile-list'>
+            {favoriteProducts.map(favorite => (
                 <FavoriteTile key={favorite.id} favorite={favorite} />
             ))}
         </div>
