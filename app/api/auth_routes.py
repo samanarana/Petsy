3309645ba86863277a0a46a3d48d48d43daf1bc4
Saman Rana <saman.ranaaa@gmail.com>
2023-10-17 15:48:11 -0500

    login and signup frontend and backend validation, profile dropdown css

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, jsonify, session, request
+from werkzeug.security import check_password_hash
 from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+import re
 
 auth_routes = Blueprint('auth', __name__)
 
@@ -40,8 +42,16 @@ def login():
     if form.validate_on_submit():
         # Add the user to the session, we are logged in!
         user = User.query.filter(User.email == form.data['email']).first()
-        login_user(user)
-        return user.to_dict()
+        if user:
+            # Check if the given password matches the hashed password in the database
+            if check_password_hash(user.password, form.data['password']):
+                login_user(user)
+                return user.to_dict()
+            else:
+                return {'errors': {'password': 'Password is incorrect'}}, 401
+        else:
+            return {'errors': {'email': 'Email not found'}}, 401
+
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
 
@@ -56,22 +66,41 @@ def logout():
 
 @auth_routes.route('/signup', methods=['POST'])
 def sign_up():
-    """
-    Creates a new user and logs them in
-    """
+    if current_user.is_authenticated:
+        return {'message': 'User already logged in.'}, 400
+
     form = SignUpForm()
     form['csrf_token'].data = request.cookies['csrf_token']
+
     if form.validate_on_submit():
+        email_regex = r"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$"
+        if not re.match(email_regex, form.data['email'], re.IGNORECASE):
+            return jsonify(errors=['Please enter a valid email address.']), 400
+
+        if len(form.data['password']) < 6:
+            return jsonify(errors=['Password should be at least 6 characters.']), 400
         user = User(
             username=form.data['username'],
             email=form.data['email'],
             password=form.data['password']
         )
+
+        existing_email = User.query.filter_by(email=form.data['email']).first()
+        existing_username = User.query.filter_by(username=form.data['username']).first()
+
+        if existing_email:
+            return {'errors': ['Email is already in use.']}, 401
+
+        if existing_username:
+            return {'errors': ['Username is already taken.']}, 401
+
         db.session.add(user)
         db.session.commit()
         login_user(user)
         return user.to_dict()
-    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+    error_messages = [error for errors in form.errors.values() for error in errors]
+    return jsonify(errors=error_messages), 401
 
 
 @auth_routes.route('/unauthorized')
--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -15,77 +15,105 @@ function SignupFormModal() {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-			}
-		} else {
-			setErrors([
-				"Confirm Password field must be the same as the Password field",
-			]);
+		let validationErrors = {};
+
+		// Email Validation
+		const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+		if (!emailRegex.test(email)) {
+            validationErrors.email = "Please enter a valid email address.";
+        }
+
+		// Password Strength
+		if (password.length < 6) {
+			validationErrors.password = "Password should be at least 6 characters.";
 		}
-	};
+
+		// Check Confirm Password
+		if (password !== confirmPassword) {
+			validationErrors.confirmPassword = "Confirm Password field must be the same as the Password field.";
+		}
+
+		// Username Length
+		if (username.length < 4) {
+			validationErrors.username = "Username should be at least 4 characters.";
+		}
+
+		if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            return;
+        }
+
+		const data = await dispatch(signUp(username, email, password));
+
+        if (data && data.errors) {
+            setErrors({ general: data.errors });
+        } else {
+            closeModal();
+        }
+    };
 
 	return (
 		<>
 		<div className="signup-form-modal">
 			<h3>Create your account</h3>
 			<p>Registration is easy.</p>
-			<form onSubmit={handleSubmit}>
-				<ul>
-					{errors.map((error, idx) => (
-						<li key={idx}>{error}</li>
-					))}
-				</ul>
-				<label>
-					Email *
-					<input
-						type="text"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Username *
-					<input
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Password *
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<label>
-					Confirm Password *
-					<input
-						type="password"
-						value={confirmPassword}
-						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<button
-					className="signup-button"
-					type="submit"
-					disabled={!email || !username || !password || !confirmPassword}>
-					Register
-				</button>
-			</form>
-		</div>
+			{errors.general && errors.general.map(error =>
+                <div key={error} className="error-message">{error}</div>
+            )}
+				<form onSubmit={handleSubmit}>
+					<label>
+						Email *
+						<input
+							type="text"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className={errors.email ? "input-error" : ""}
+							required
+						/>
+						{errors.email && <div className="error-message">{errors.email}</div>}
+					</label>
+					<label>
+						Username *
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							className={errors.username ? "input-error" : ""}
+							required
+						/>
+						{errors.username && <div className="error-message">{errors.username}</div>}
+					</label>
+					<label>
+						Password *
+						<input
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							className={errors.password ? "input-error" : ""}
+							required
+						/>
+						{errors.password && <div className="error-message">{errors.password}</div>}
+					</label>
+					<label>
+						Confirm Password *
+						<input
+							type="password"
+							value={confirmPassword}
+							onChange={(e) => setConfirmPassword(e.target.value)}
+							className={errors.confirmPassword ? "input-error" : ""}
+							required
+						/>
+						{errors.confirmPassword && <div className="error-message">{errors.confirmPassword}</div>}
+					</label>
+					<button
+						className="signup-button"
+						type="submit"
+						disabled={!email || !username || !password || !confirmPassword}>
+						Register
+					</button>
+				</form>
+			</div>
 		</>
 	);
 }
-
 export default SignupFormModal;
