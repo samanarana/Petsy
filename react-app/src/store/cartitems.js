9073ad8248290c509553cce4508cf20483b5dd00
samanarana <113636092+samanarana@users.noreply.github.com>
2023-10-27 17:07:49 -0500

    Revert "Revert 59 saman10/26"

--- react-app/src/store/cartitems.js
+++ react-app/src/store/cartitems.js
@@ -1,13 +1,8 @@
-// Local Storage -- Cart Persistence
-const savedCart = localStorage.getItem('currentCart');
-const initialState = {
-    currentCart: savedCart ? JSON.parse(savedCart) : [],
-};
-
 // Action Types
 const GET_CART_ITEMS = "cart/GET_CART_ITEMS";
 const ADD_TO_CART = "cart/ADD_TO_CART";
 const REMOVE_CART_ITEM = "cart/REMOVE_CART_ITEM";
+const CLEAR_CART = "cart/CLEAR_CART"
 
 //Action Creators
 const getCartItems = (cartItems) => ({
@@ -24,6 +19,11 @@ const removeFromCart = (itemId) => ({
     type: REMOVE_CART_ITEM,
     payload: itemId,
 });
+
+const clearCart = () => ({
+    type: CLEAR_CART,
+    payload: null
+})
 
 //Thunks
 export const fetchCartItemsThunk = () => async (dispatch) => {
@@ -61,49 +61,50 @@ export const removeFromCartThunk = (itemId) => async (dispatch) => {
     }
 };
 
+export const clearCartThunk = () => async (dispatch) => {
+    const response = await fetch(`/api/cart/clear`, {
+        method: "DELETE"
+    });
+
+    if (response.ok) {
+        dispatch(clearCart())
+    };
+}
+
+// State
+
+const initialState = {
+    currentCart: [],
+};
+
 
 //Reducer
 
 function cartReducer(state = initialState, action) {
     let newState = {...state};
 
-    //breaks after are required or we won't be hitting the code that allows us to put things in local storage.
-
     switch (action.type) {
         case GET_CART_ITEMS:
             newState.currentCart = action.payload;
             return newState;
-
-            break;
         case ADD_TO_CART:
-            newState.currentCart.push(action.payload);
+            newState.currentCart = [...newState.currentCart, action.payload];
             return newState;
-
-            break;
         case REMOVE_CART_ITEM:
             const productIdToRemove = action.payload;
-            const cartItemIndex = newState.currentCart.findIndex(item => item.productId === productIdToRemove);
-            const cartItem = newState.currentCart[cartItemIndex];
-        
-            if (cartItem && cartItem.quantity > 1) {
-                newState.currentCart[cartItemIndex].quantity -= 1;
-            }
-            else if (cartItem) {
-                newState.currentCart.splice(cartItemIndex, 1);
-            }
+            newState.currentCart = newState.currentCart.map(item => {
+                if (item.productId === productIdToRemove && item.quantity > 1) {
+                    return { ...item, quantity: item.quantity - 1 };
+                }
+                return item;
+            }).filter(item => !(item.productId === productIdToRemove && item.quantity <= 1));
             return newState;
-
-            break;
-            
+        case CLEAR_CART:
+            newState.currentCart = [];
+            return newState;
         default:
             return state;
-    }
-
-    // Save the current cart to localStorage after any change
-    localStorage.setItem('currentCart', JSON.stringify(newState.currentCart));
-    return newState;
+    };
 };
 
-
-
-  export default cartReducer;
+export default cartReducer;
