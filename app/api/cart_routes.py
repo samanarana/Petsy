af5f752193cbe77d69008dce95f8b20ed0d4824c
samanarana <113636092+samanarana@users.noreply.github.com>
2023-10-27 16:48:20 -0500

    Revert "Saman10/26"

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -3,7 +3,9 @@ from flask_login import login_required, current_user
 from app.models import CartItem
 from app import db
 
+
 cart_routes = Blueprint('cart', __name__)
+
 
 # Retrieve all products in the user's cart
 @cart_routes.route('/', methods=['GET'])
@@ -20,13 +22,15 @@ def view_cart():
 def add_to_cart():
 
     data = request.get_json()
+    print(data)
     if "productId" not in data or "quantity" not in data or "price" not in data:
-        return jsonify(status="error", error_type="data_missing", message="Required data not provided"), 400
+        return jsonify({"error": "Missing data"}), 400
 
     # Check if the item is already in the cart and update quantity
     cart_item = CartItem.query.filter_by(
         userId=current_user.id, productId=data["productId"]
     ).first()
+
     if cart_item:
         cart_item.quantity += data["quantity"]
     else:
@@ -39,8 +43,7 @@ def add_to_cart():
 
     db.session.add(cart_item)
     db.session.commit()
-    return jsonify(status="success", message="Item added to cart successfully", data={}), 201
-
+    return jsonify({"message": "Item added to cart successfully"}), 201
 
 
 
@@ -51,15 +54,17 @@ def remove_from_cart(productId):
     cart_item = CartItem.query.filter_by(
         userId=current_user.id, productId=productId
     ).first()
+
     if not cart_item:
-        return jsonify(status="error", error_type="not_found", message="Item not found"), 404
+        return jsonify(success=False, message="Item not found")
+
     if cart_item.quantity > 1:
         cart_item.quantity -= 1
     else:
         db.session.delete(cart_item)
-    db.session.commit()
-    return jsonify(status="success", message='Item updated in cart', data={}), 200
 
+    db.session.commit()
+    return jsonify(success=True, message='Item updated in cart')
 
 
 
@@ -67,17 +72,10 @@ def remove_from_cart(productId):
 @cart_routes.route('/transaction', methods=['POST'])
 @login_required
 def complete_transaction():
+
+    # transaction logic here
+
     CartItem.query.filter_by(userId=current_user.id).delete()
     db.session.commit()
-    return jsonify(status="success", message="Transaction completed", data={}), 200
 
-
-# Clear the cart
-@cart_routes.route('/clear', methods=['DELETE'])
-@login_required
-def delete_all_items():
-    CartItem.query.filter_by(userId=current_user.id).delete()
-    db.session.commit()
-    return jsonify(status="success", message="Cart cleared", data={}), 200
-
-
+    return {'message': 'Transaction completed'}, 200
