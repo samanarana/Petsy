071631173fbdd75b642aecaf89747cde7dc15a76
“Carlos <carlosjstich@gmail.com>
2023-10-28 15:46:43 -0500

    added a basic pop up for delete review

--- app/seeds/orderitems.py
+++ app/seeds/orderitems.py
@@ -5,13 +5,15 @@ def seed_orderitems():
 
     orderitem1 = OrderItem(orderId=1, productId=1)
     orderitem2 = OrderItem(orderId=1, productId=2)
+
     orderitem3 = OrderItem(orderId=2, productId=3)
     orderitem4 = OrderItem(orderId=3, productId=4)
     orderitem5 = OrderItem(orderId=4, productId=1)
     orderitem6 = OrderItem(orderId=5, productId=2)
     orderitem7 = OrderItem(orderId=5, productId=5)
+    orderitem8 = OrderItem(orderId=1, productId=4)
 
-    orderitems = [orderitem1, orderitem2, orderitem3, orderitem4, orderitem5, orderitem6, orderitem7]
+    orderitems = [orderitem1, orderitem2, orderitem3, orderitem4, orderitem5, orderitem6, orderitem7, orderitem8]
 
     db.session.add_all(orderitems)
     db.session.commit()
